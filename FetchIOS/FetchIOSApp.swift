d8de50441b626100cd013ab8fa683dca4459f96b
TarunVedula <98503523+TarunVedula@users.noreply.github.com>
2024-10-17 17:24:27 -0500

    Initial Commit

---
+++ FetchIOS/FetchIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchIOSApp.swift
+//  FetchIOS
+//
+//  Created by Tarun Vedula on 10/17/24.
+//
+
+import SwiftUI
+
+@main
+struct FetchIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FetchIOSTests/FetchIOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  FetchIOSTests.swift
+//  FetchIOSTests
+//
+//  Created by Tarun Vedula on 10/17/24.
+//
+
+import Testing
+@testable import FetchIOS
+
+struct FetchIOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
